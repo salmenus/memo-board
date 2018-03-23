c02239a95be2194cba0448000b324378b2bd9c02
Salmen Hichri <salmen.hichri@gmail.com>
2018-03-23 22:13:42 +0000

    Added base CSS styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './reset.css';
+
+import './style/reset.css';
 import './index.css';
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
