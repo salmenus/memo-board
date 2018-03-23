41b2e5fbc0cf0783d5fde3aee21fc42f5f682372
Salmen Hichri <salmen.hichri@gmail.com>
2018-03-23 23:48:32 +0000

    Improved layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={'app'}>
         <header>
           <h1>Memo Board</h1>
         </header>
