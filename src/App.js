7cb5b44c1f15396e5e36d036345558d693cb583e
Salmen Hichri <salmen.hichri@gmail.com>
2018-03-23 23:40:54 +0000

    Added high level app layout

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Memo Board</h1>
+        <header>
+          <h1>Memo Board</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          Main
+        </main>
+        <footer>
+          Footer
+        </footer>
       </div>
     );
   }
