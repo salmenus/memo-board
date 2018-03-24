2a73d16895f124daf57e92c46970a56b32f340a1
Salmen Hichri <salmen.hichri@gmail.com>
2018-03-24 13:33:09 +0000

    Added flexbox style for memos

---
+++ src/components/Memo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Memo.css'
+
+class Memo extends Component {
+
+  render() {
+    return (<div className={'memo'}>One memo</div>);
+  }
+}
+
+export default Memo;
--- src/components/Memos.js
+++ src/components/Memos.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Memo from './Memo';
 import './Memos.css'
 
 class Memos extends Component {
   render() {
-    return (<div className={'memos'}>Memos</div>);
+    const memos = [<Memo key={1}/>, <Memo key={2}/>, <Memo key={3}/>, <Memo key={4}/>, <Memo key={5}/>, <Memo key={6}/>, <Memo key={7}/>];
+    return (<div className={'memos'}>{memos}</div>);
   }
 }
 
