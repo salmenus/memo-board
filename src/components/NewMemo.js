b5331c56c798c6a7f988a292b9f5872e2a02929a
Salmen Hichri <salmen.hichri@gmail.com>
2018-03-24 17:08:55 +0000

    Added new memo button

---
+++ src/components/NewMemo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './NewMemo.css'
+
+class NewMemo extends Component {
+
+  render() {
+    return <button className={'new-memo-button'}>XXX</button>
+  }
+}
+
+export default NewMemo;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { t } from 'i18next';
+import NewMemo from './../NewMemo';
 import './Header.css';
 
 const Header = () => {
   return (
     <header className={'header'}>
-      <h1>{t('app name')}</h1>
+      <h1 className={'title'}>{t('app name')}</h1>
+      <div className={'actions'}>
+        <NewMemo />
+      </div>
     </header>);
 };
 
