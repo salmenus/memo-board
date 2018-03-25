c743a746e137ffe615589e89d6e48657c060a764
Salmen Hichri <salmen.hichri@gmail.com>
2018-03-25 12:47:43 +0100

    Implemented logic to update memos in store when a new memo is added

--- src/data/createNewMemo.js
+++ src/data/createNewMemo.js
@@ -3,21 +3,20 @@ import '@firebase/firestore';
 
 export default async ({firebase = Firebase, store = null}) => {
 
+  const creationDate = new Date();
   const memoReference = await firebase.firestore().collection('memos').add({
-    creation_data: new Date()
+    creation_data: creationDate
   });
 
   const newMemo = {
     id: memoReference.id,
-    creationDate: memoReference.data().creation_data
+    creationDate: creationDate
   };
-
-  console.dir(newMemo);
 
   if(store && typeof store.dispatch === 'function') {
     store.dispatch({
       type: 'ADD_MEMO',
-      newMemo
+      memo: newMemo
     });
   }
 
--- src/reducers/memos.js
+++ src/reducers/memos.js
@@ -1,12 +1,19 @@
 const memos = (state = null, action) => {
   switch (action.type) {
-    case 'UPDATE_MEMOS':
+    case 'UPDATE_MEMOS': {
       return Array.isArray(action.memos) ? [...action.memos] : [];
-    case 'UPDATE_MEMO':
+    }
+    case 'ADD_MEMO': {
+      const newMemo = {...action.memo};
+      return [newMemo, ...state];
+    }
+    case 'UPDATE_MEMO': {
       const memo = action.memo;
-      return { id: memo.id, title: memo.title, body: memo.body,  creationDate: memo.creationDate};
-    default:
+      return {id: memo.id, title: memo.title, body: memo.body, creationDate: memo.creationDate};
+    }
+    default: {
       return state;
+    }
   }
 };
 
