c80c7e646274092c23be7a8622410d17a4d36e6e
Salmen Hichri <salmen.hichri@gmail.com>
2018-03-25 19:30:32 +0100

    Improved unit tests

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,6 +1,8 @@
-export default {
+const en = {
   'app name': 'Memo Board',
   'footer message': 'Copyright (c) 2018 Salmen Hichri',
   'create new memo': 'New memo',
   'delete memo': 'Delete'
 };
+
+export default en;
--- src/reducers/memo.test.js
+++ src/reducers/memo.test.js
@@ -7,11 +7,11 @@ it('returns default state when it receives invalid action', () => {
   expect(result).toBe(defaultState);
 });
 
-it('adds new memo to state when it receives a valid ADD_MEMO action', () => {
-  const defaultState = [{id: 'default id', title: 'default title', body: 'default body', creationDate: null}];
-  const newMemo = {id: 100, title: 'My memos!', body: 'Let us do 100% test coverage!', creationDate: new Date()};
-  const result = memoReducer(defaultState, {type: 'ADD_MEMO', memo: newMemo});
-  expect(result).toEqual([newMemo, ...defaultState]);
+it('adds new memo to state and updates most recent attribute when it receives a valid ADD_MEMO action', () => {
+  const initialMemo = [{id: 'default id', title: 'default title', body: 'default body', creationDate: null, mostRecent: true}];
+  const newMemo = {id: 100, title: 'My memos!', body: 'Let us do 100% test coverage!', creationDate: new Date(), mostRecent: true};
+  const result = memoReducer([initialMemo], {type: 'ADD_MEMO', memo: newMemo});
+  expect(result).toEqual([newMemo, {...initialMemo, mostRecent: false}]);
 });
 
 it('updates memos state when it receives a valid UPDATE_MEMO action', () => {
