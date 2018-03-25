fa5182b6d45fd18d334b1efd1eeae0254d398bd3
Salmen Hichri <salmen.hichri@gmail.com>
2018-03-25 17:31:39 +0100

    Updated app header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,7 +6,10 @@ import './Header.css';
 const Header = () => {
   return (
     <header className={'header'}>
-      <h1 className={'title'}>{t('app name')}</h1>
+      <h1 className={'title'}>
+        <span className={'logo'} />
+        <span>{t('app name')}</span>
+        </h1>
       <div className={'actions'}>
         <NewMemo />
       </div>
