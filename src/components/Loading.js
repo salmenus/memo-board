08c98874d398a673cf0f432b13e9824f6f19503d
Salmen Hichri <salmen.hichri@gmail.com>
2018-03-25 12:23:57 +0100

    Added loading spinner component

--- src/components/Loading.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { t } from 'i18next';
-
-export default () => {
-  return <span>{ t('loading message') }</span>
-}
---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './LoadingSpinner.css'
+
+export default () => (
+  <div className={'loading-spinner'}>
+    <div className={'dot-1'}></div>
+    <div className={'dot-2'}></div>
+  </div>
+);
