444eea3c84f521e1acb684512124103b5220be7b
Salmen Hichri <salmen.hichri@gmail.com>
2018-03-26 03:08:34 +0100

    Fixed failing unit tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,9 +2,23 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 jest.mock('i18next', () => ({
-  // this mock makes sure any components using the translate HoC receive the t function as a prop
   init: jest.fn(),
   t: () => jest.fn().mockReturnValue('t')
 }));
+
+jest.mock('@firebase/app', () => ({
+  firestore: jest.fn().mockReturnValue({
+    collection: jest.fn().mockReturnValue({
+      get: jest.fn(),
+      add: jest.fn(),
+      doc: jest.fn().mockReturnValue({
+        update: jest.fn(),
+        delete: jest.fn()
+      })
+    })
+  })
+}));
+
+jest.mock('@firebase/firestore', () => ({}));
 
 configure({ adapter: new Adapter() });
